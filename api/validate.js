48972da579e32e5b7e55237d57a9c060cb7967f7
classroom84x-games <Classroom84X@outlook.com>
2025-03-08 01:29:53 -0500

    Create validate.js

---
+++ api/validate.js
@@ -0,0 +1,17 @@
+let generatedPasscodes = new Set();
+
+export default function handler(req, res) {
+    if (req.method === 'POST') {
+        const { passcode } = req.body;
+
+        if (generatedPasscodes.has(passcode)) {
+            generatedPasscodes.delete(passcode);
+            return res.status(200).json({ valid: true });
+        } else {
+            return res.status(400).json({ valid: false });
+        }
+    } else {
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
